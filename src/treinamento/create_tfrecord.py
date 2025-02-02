a05e9b2b0ab20dbbbeac40fe0adaa6ae664b15e6
Sergio Venancio <svenancio@gmail.com>
2025-02-02 18:45:21 -0300

    Update create_tfrecord.py

--- src/treinamento/create_tfrecord.py
+++ src/treinamento/create_tfrecord.py
@@ -1,4 +1,3 @@
-# Script from: https://raw.githubusercontent.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/master/util_scripts/create_tfrecord.py
 # Script to create TFRecord files from train and test dataset folders
 # Originally from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/generate_tfrecord.py
 
